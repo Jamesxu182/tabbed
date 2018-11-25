47e8f1aa8b54928ee1b582b3c2ee79adfe0afb8c
Yifan Xu <xuy7@tcd.ie>
2018-11-25 11:57:55 +0000

    Change to i3wm navigation shortcut

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static       Bool urgentswitch  = False;
 /*
  * Where to place a new tab when it is opened. When npisrelative is True,
  * then the current position is changed + newposition. If npisrelative
- * is False, then newposition is an absolute position.
+ * is False, then newposition is an abssemicolonolute position.
  */
 static int  newposition   = 0;
 static Bool npisrelative  = False;
@@ -42,11 +42,15 @@ static Key keys[] = {
     { MODKEY,               XK_Return, spawn,       { 0 } },
 
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	// { MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
+	// { MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
+	// { MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
+	// { MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+    { MODKEY,               XK_j,           rotate,     { .i = -1 } },
+    { MODKEY,               XK_semicolon,   rotate,     { .i = +1 } },
+    { MODKEY|ShiftMask,     XK_j,           movetab,     { .i = -1 } },
+    { MODKEY|ShiftMask,     XK_semicolon,   movetab,     { .i = +1 } },
+	{ MODKEY,               XK_Tab,         rotate,     { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
